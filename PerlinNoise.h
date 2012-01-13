c02865cfc43d8fe2d9b66f3985d8aae205c61247
termoose <birkedal@stud.ntnu.no>
2012-01-13 09:29:32 +0100

    Constness. Removed BridgeRunner in header guards

--- PerlinNoise.h
+++ PerlinNoise.h
@@ -1,5 +1,5 @@
-#ifndef BridgeRunner_PerlinNoise_h
-#define BridgeRunner_PerlinNoise_h
+#ifndef PerlinNoise_h
+#define PerlinNoise_h
 
 // 1D Perlin noise class for terrain generation
 class PerlinNoise
@@ -8,13 +8,13 @@ public:
     PerlinNoise( float Persistance, int NumberOfOctaves );
     ~PerlinNoise();
     
-    float GetNoise( float x );
+    float GetNoise( float x ) const;
     
 private:
-    float Noise( int x );
-    float SmoothNoise( float x );
-    float CosineInterpolation( float a, float b, float x );
-    float InterpolatedNoise( float x );
+    float Noise( int x ) const;
+    float SmoothNoise( float x ) const;
+    float CosineInterpolation( float a, float b, float x ) const;
+    float InterpolatedNoise( float x ) const;
     
     float p;
     int n;
