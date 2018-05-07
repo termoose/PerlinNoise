ba4030a4bb4686e21a1007bce4610beb41c360db
Ole Andre Birkedal <o.birkedal@sportradar.com>
2018-05-07 16:12:07 +0200

    Header only, snake_case for functions/variables, syntax changes

--- PerlinNoise.h
+++ PerlinNoise.h
@@ -1,24 +1,60 @@
-#ifndef PerlinNoise_h
-#define PerlinNoise_h
+#pragma once
+
+#include <cmath>
 
 // 1D Perlin noise class for terrain generation
 class PerlinNoise
 {
 public:
-    PerlinNoise( float Persistance, int NumberOfOctaves );
-    ~PerlinNoise();
-    
-    float GetNoise( float x ) const;
-    
-private:
-    float Noise( int x ) const;
-    float SmoothNoise( float x ) const;
-    float CosineInterpolation( float a, float b, float x ) const;
-    float InterpolatedNoise( float x ) const;
-    
-    float p;
-    int n;
+ PerlinNoise(float persistance, int number_of_octaves)
+   : p(persistance), n(number_of_octaves) {}
 
+  float get_noise(float x) const
+  {
+    float total = 0.0;
+
+    for(unsigned int i = 0; i < n; ++i)
+    {
+      unsigned int freqency = 1<<i; // 2^i
+      float amplitude = pow(p, i);
+
+      total += interpolated_noise(x * freqency) * amplitude;
+    }
+
+    return total;
+  }
+
+ private:
+  float noise(int x) const
+  {
+    x = (x << 13) ^ x;
+    return (1.0 - ((x*(x*x*15731+789221)+1376312589) & 0x7fffffff) / 1073741824.0);
+  }
+
+  float smooth_noise(float x) const
+  {
+    return noise(x)*0.5 + (noise(x - 1) + noise(x + 1))*0.25;
+  }
+
+  float cosine_interpolation(float a, float b, float x) const
+  {
+    float ft = x * M_PI;
+    float f  = (1 - cos( ft )) * 0.5;
+    
+    return  a * (1 - f) + b * f;
+  }
+
+  float interpolated_noise(float x) const
+  {
+    double frac_part, int_part;
+    frac_part = modf(x, &int_part);
+
+    float v1 = smooth_noise(int_part);
+    float v2 = smooth_noise(int_part + 1);
+
+    return cosine_interpolation(v1, v2, frac_part);
+  }
+
+  float p;
+  int n;
 };
-
-#endif
